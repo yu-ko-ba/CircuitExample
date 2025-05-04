e65386ca302c460d34c71c677a227ac3e665679c
yuyuyuyuyu <mail@yuyuyuyuyu.dev>
2025-05-04 13:56:06 +0900

    build: add dependencies

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -40,6 +40,8 @@ kotlin {
             implementation(compose.components.uiToolingPreview)
             implementation(libs.androidx.lifecycle.viewmodel)
             implementation(libs.androidx.lifecycle.runtime.compose)
+
+            implementation(libs.circuit)
         }
     }
 }
